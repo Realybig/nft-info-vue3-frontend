9ab418ac98054a94834d50762ade74002921727f
dieselhuang <dieselhuang@tencent.com>
2022-12-10 22:17:06 +0800

    feat: 新增路由相关组件页面及代码

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,30 @@
 import { createApp } from 'vue'
+import { createRouter, createWebHashHistory } from 'vue-router'
 import './style.css'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+// 导入组件
+import Home from './components/Home.vue'
+import About from './components/About.vue'
+
+// 定义路由
+const routes = [
+    { path: '/', component: Home },
+    { path: '/about', component: About },
+]
+
+// 加载路由
+const router = createRouter({
+    // 路由模式为 History
+    history: createWebHashHistory(),
+    routes,
+})
+
+// 创建应用实例
+const app = createApp(App)
+
+// 使用路由实例
+app.use(router)
+
+// 挂载应用
+app.mount('#app')
